21e440d77e222b6a4cd3aecb97a5f417edc51765
kiemsibat <hoanglan1989vn@gmail.com>
2020-09-10 15:09:59 +0700

    Section4

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Button,TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   const name = "Lân Hoàng";
+  console.log(navigation)
   return (
     <View>
       <Text style={styles.text}>Getting started with react native! </Text>
       {/* {greeting} */}
-  <Text style={styles.text1}>My name is : {name}</Text>
+      <Text style={styles.text1}>My name is : {name}</Text>
+      <Button 
+      onPress={() => navigation.navigate('Components')}
+      title="Go to components Demo"
+      style={styles.Button}
+      />
+
+      {/*  */}
+      <Button
+        title="Go to list demo"
+        onPress={() => navigation.navigate('List')}
+        style={styles.Button}
+      />
+      {/*  */}
     </View>
   );
 };
@@ -20,6 +34,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "blue",
   },
+  Button:{
+    marginVertical:50,
+    backgroundColor:'yellow'
+  }
 });
 
 export default HomeScreen;
