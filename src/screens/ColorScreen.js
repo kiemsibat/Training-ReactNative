04822be8eba3beb07829c07f3908558a539079b7
kiemsibat <hoanglan1989vn@gmail.com>
2020-09-10 23:58:31 +0700

    Update Complete

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,34 +1,91 @@
 import React,{useState} from "react";
-import {  StyleSheet, View,Button, FlatList} from 'react-native';
+import {  StyleSheet, View,Button, Text} from 'react-native';
 
 const ColorScreen = () => {
-    const [color,setColor] = useState([]);
-    const randomRgb = () => {
-        const red = Math.floor(Math.random()*256);
-        const green = Math.floor(Math.random()*256);
-        const blue = Math.floor(Math.random()*256);
-        return `rgb(${red},${green},${blue})`
-    }
+    const [red,setRed] = useState(0);
+    const [green,setGreen] = useState(0);
+    const [blue,setBlue] = useState(0);
+
     return(
        <View>
+           <Text>
+               Red
+           </Text>
             <Button 
-            onPress={() => setColor([...color,randomRgb()])} 
-            title="Add a Color"
+            onPress={() => {
+                setRed(red + 5);
+                if(red > 250){
+                    setRed(0);
+                }
+                console.log(red);
+            }} 
+            title="More Red"
             />
-          
-            <FlatList
-                keyExtractor={(item) => item}
-                data={color}
-                renderItem={({ item}) =>  {
-                    return   <View style={{height:100,backgroundColor: item}}/>
-                }}
+             <Button 
+            onPress={() =>{
+                console.log(red);
+                setRed(red - 5)
+                if(red < 5){
+                    setRed(250);
+                }
+            }} 
+            title="Less Red"
             />
+              <Text>
+               Green
+           </Text>
+             <Button 
+            onPress={() => {
+                setGreen(green + 5)
+                if(green > 250){
+                    setGreen(0);
+                }
+            }} 
+            title="More Green"
+            />
+             <Button 
+            onPress={() => {
+                setGreen(green - 5)
+                if(green < 5){
+                    setGreen(255);
+                }
+            }} 
+            title="Less Green"
+            />
+              <Text>
+               Blue
+           </Text>
+             <Button 
+            onPress={() =>{
+                 setBlue(blue + 5)
+                if(blue > 250){
+                    setBlue(0);
+                }
+                }} 
+            title="More Blue"
+            />
+             <Button 
+            onPress={() =>{
+                 setBlue(blue - 5)
+                 if(blue < 5){
+                    setBlue(255);
+                }
+                }} 
+            title="Less Blue"
+            />
+        <View
+            style={{height:30,backgroundColor:`rgb(${red},${green},${blue})`}}
+        />      
+        <Text>{`rgb(${red},${green},${blue})`}</Text>
        </View>
-    )
 
+    )
 }
 
 const styles = StyleSheet.create({
+    View: {
+        backgroundColor:'blue'
+    }
 })
 
 export default ColorScreen;
