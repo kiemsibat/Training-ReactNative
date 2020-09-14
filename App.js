4a97efb94732beeff60e1b9ae86b4f1447c49d67
kiemsibat <hoanglan1989vn@gmail.com>
2020-09-14 11:55:33 +0700

    Section 7

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "Home"
+      title: "App"
     }
   }
 );
--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,35 +3,74 @@ import { Text, StyleSheet, View,} from 'react-native';
 
 const BoxScreen = () => {
     return(
-        <View style={styles.Box}>
-            <Text style={styles.Text}>BoxScreen</Text>
-            <Text style={styles.Text}>BoxScreen1</Text>
-            <Text style={styles.Text}>BoxScreen2</Text>
-            
+       <>
+         <View style={styles.Box}>
+            <View style={styles.Text1}/>
+            <View style={styles.Text2}/>
+            <View style={styles.Text3}>
+       
+       </View>        
         </View>
+       
+       </>
+        
     )
 }
 
-
 const styles = StyleSheet.create({
     Box: {
-      
         borderWidth:1,
-        borderColor:'yellow',
+        height:300,
+        // borderColor:'yellow',
         // alignItems: 'center'
         flexDirection:'row',
-        justifyContent:'space-between'
+        justifyContent:'space-between',
     },
-    Text:{
-        borderColor:'red',
+    
+    Text1:{
+       
+        height:50,
+        width:50,
+        // flex:1,
         borderWidth:1,
         marginVertical:10,
         color:'white',
         fontSize:20,
         backgroundColor:'blue',
+        textAlign: 'center',
+        borderColor:'red'
+    },
+    Text2:{
+        borderColor:'red',
+        height:50,
+        width:50,
+        // flex:2,
+        borderWidth:1,
+        marginVertical:10,
+        color:'white',
+        fontSize:20,
+        backgroundColor:'yellow',
+        textAlign: 'center',
+        borderColor:'red',
+        top:50,
      
-        textAlign: 'center'
-    }
+    },
+    Text3:{
+        borderColor:'red',
+        height:50,
+        width:50,
+        borderColor:'red',
+        // flex:1,
+      
+        borderWidth:1,
+        marginVertical:10,
+        color:'white',
+        fontSize:20,
+        backgroundColor:'red',
+        justifyContent:'space-between',
+
+    },
+  
 })
 
 export default BoxScreen
