5a04aa47af7cbbd80907703d82b9c1df3fed410e
kiemsibat <hoanglan1989vn@gmail.com>
2020-09-18 17:43:53 +0700

    input props

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Input, Text, Button } from "react-native-elements";
 import Spacer from "../../src/components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+    const[userName,setUserName] = useState('');
+    const[password,setPassword] = useState('');
+
   return (
-    <View>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Signup for tracker</Text>
       </Spacer>
@@ -13,6 +16,11 @@ const SignupScreen = ({ navigation }) => {
         <Input
           // placeholder="Username"
           label="Username"
+          value={userName}
+          onChangeText={(text) => {
+            setUserName(text)
+            console.log(text)
+          }}
         />
       </Spacer>
       <Spacer>
@@ -20,6 +28,11 @@ const SignupScreen = ({ navigation }) => {
           //   placeholder="Password"
           label="Password"
           secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => {
+              setPassword(text)
+              console.log(text)
+            }}
         />
       </Spacer>
 
@@ -32,6 +45,16 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
+  container:{
+      flex:1,
+      justifyContent:'center',
+      marginBottom:10
+  }
 });
 
+SignupScreen.navigationOptions = () => {
+    return{
+        header:() => false
+    };
+};
 export default SignupScreen;
