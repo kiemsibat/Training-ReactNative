fb73c9d112443251ab1fca4ec0dbf0bd150475bf
kiemsibat <hoanglan1989vn@gmail.com>
2020-09-21 16:18:54 +0700

    creating nav link

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,27 +1,27 @@
-import React, {useContext} from "react";
-import { View, StyleSheet ,TouchableOpacity} from "react-native";
-import {Text } from "react-native-elements";
-import Spacer from "../../src/components/Spacer";
-import {Context as AuthContext} from "../context/AuthContext"
-import AuthForm from '../components/AuthForm'
-import {AsyncStorage} from 'react-native';
+import React, { useContext } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+// import { Text } from "react-native-elements";
+// import Spacer from "../../src/components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+// import { AsyncStorage } from "react-native";
+import NavLink from '../components/NavLink'
 const SignupScreen = ({ navigation }) => {
-    const {state,signup} = useContext(AuthContext);
+  const { state, signup } = useContext(AuthContext);
 
-    console.log('state is here',state);
+  console.log("state is here", state);
   return (
     <View style={styles.container}>
       <AuthForm
         headerText="Sign Up for Tracker"
-        errorMessage={state.errorMessage}      
+        errorMessage={state.errorMessage}
         onSubmit={signup}
         submitButtonText="Sign Up"
       />
-      <Spacer>
-        <Text onPress={() => navigation.navigate('Signin')} style={styles.link}>
-          Already have an account ? Sign in instead 
-        </Text>
-      </Spacer>
+      <NavLink
+        text="Already have an account ? Sign In"
+        routeName="Signin"
+      />
     </View>
   );
 };
@@ -30,25 +30,21 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
-  container:{
-      flex:1,
-      justifyContent:'center',
-      marginBottom:10,
-      backgroundColor:'#A8D1DE'
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 10,
+    backgroundColor: "#A8D1DE",
   },
-  error:{
-      color:'red',
-      fontSize:20
+  error: {
+    color: "red",
+    fontSize: 20,
   },
-  link:{
-    color:'blue',
-
-  }
 });
 
 SignupScreen.navigationOptions = () => {
-    return{
-        header:() => false
-    };
+  return {
+    header: () => false,
+  };
 };
 export default SignupScreen;
