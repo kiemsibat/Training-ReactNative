4f3b61b8c960d096c33e78baa3a6fde71b15abec
kiemsibat <hoanglan1989vn@gmail.com>
2020-09-22 11:14:18 +0700

    updated empty screens whitle resolving auth

--- App.js
+++ App.js
@@ -15,8 +15,10 @@ import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
 import {Provider as AuthProvider} from './src/context/AuthContext'
 import {setNavigator} from './src/navigationRef'
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen'
 
 const switchNavigator = createSwitchNavigator({
+  ResolveAuth:ResolveAuthScreen,
   loginFlow:createStackNavigator({
     Signup:SignupScreen,
     Signin:SigninScreen,
@@ -29,6 +31,8 @@ const switchNavigator = createSwitchNavigator({
     TrackCreate:TrackCreateScreen,
     Account:AccountScreen,
   })
+},{
+   
 })
 
 const App = createAppContainer(switchNavigator);
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,28 +2,25 @@ import React, { useContext, useEffect } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
-import NavLink from '../components/NavLink'
+import NavLink from "../components/NavLink";
 import { NavigationEvents } from "react-navigation";
 const SignupScreen = () => {
-  const { state, signup,clearErrorMessage,tryLocalSignin} = useContext(AuthContext);
-  
-  useEffect(() => {
-    tryLocalSignin();
-  },[])
-
+  const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(
+    AuthContext
+  );
+  // useEffect(() => {
+  //   tryLocalSignin();
+  // },[])
   return (
     <View style={styles.container}>
-       <NavigationEvents onWillBlur={clearErrorMessage} />
+      <NavigationEvents onWillBlur={clearErrorMessage} />
       <AuthForm
         headerText="Sign Up for Tracker"
         errorMessage={state.errorMessage}
         onSubmit={signup}
         submitButtonText="Sign Up"
       />
-      <NavLink
-        text="Already have an account ? Sign In"
-        routeName="Signin"
-      />
+      <NavLink text="Already have an account ? Sign In" routeName="Signin" />
       {/* {console.log('signup',state.errorMessage)} */}
     </View>
   );
